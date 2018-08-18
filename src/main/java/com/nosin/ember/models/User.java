d103d880b76295d12aa9c370ba93c491bb8328a9
Ken Girard <ken.girard@gmail.com>
2018-08-18 12:50:07 -0500

    Got rid of email, added in fleshed out public User

--- src/main/java/com/nosin/ember/models/User.java
+++ src/main/java/com/nosin/ember/models/User.java
@@ -3,8 +3,6 @@ package com.nosin.ember.models;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.List;
-import javax.validation.constraints.Email;
 
 
 @Entity
@@ -18,14 +16,15 @@ public class User {
     @Size(min= 5, max= 15, message = "Must be between 5-15 characters")
     private String username;
 
-    @NotNull(message = "Please enter your email")
-    @Email
-    private String email;
-
     @NotNull(message = "Please enter your password.")
     private String password;
 
     public User() { }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
     public int getId() {
         return id;
@@ -37,14 +36,6 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getPassword() {
